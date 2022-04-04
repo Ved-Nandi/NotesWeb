210546808e6934fa0f1f48d4a7c24b51ce34404a
Vedant Nandi <v.nandi14@gmail.com>
2022-04-04 10:44:14 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { Login } from "./container/login/index";
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
